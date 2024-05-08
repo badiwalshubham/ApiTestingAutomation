332981d50dc379a251195b56c2f52bb05bdcfd7a
Shubham Badwial <shubhambadiwal8520@gmail.com>
2024-05-08 21:12:04 +0530

    Understood the Get Method with one and multiple user check

--- cypress/e2e/Api_testing_.cy.js
+++ cypress/e2e/Api_testing_.cy.js
@@ -7,23 +7,42 @@
 
 /// <reference types="cypress" />
 
-describe('API Automation using Cypress', ()=> {
+describe('API Automation using Cypress', () => {
 
-// Get method Api testing
+    // Get method Api testing
 
-it('Get Users', ()=> {
+    it('Get Users', () => {
 
-    cypress.request({
-        method:'GET',
-        url:'https://gorest.co.in/public/v2/users',
-        headers:{
-            Authorization:"Bearer 78917e46595309accc116964733d049c33d5f78f9caf186981cd757250cf0367"
-        }
+        cy.request({
+            method: 'GET',
+            url: 'https://gorest.co.in/public/v2/users',
+            headers: {
+                Authorization: "Bearer 78917e46595309accc116964733d049c33d5f78f9caf186981cd757250cf0367"
+            }
+        })
+            .then((response) => {
+                cy.log(JSON.stringify(response))
+                expect(response.status).to.eq(200)
+            })
+
     })
-    .then((response)=>{
-      expect(response.status).to.eq(200)
+
+    // Lets Create for validating only single user
+    it('Get User1 only', () => {
+
+        cy.request({
+            method: 'GET',
+            url: 'https://gorest.co.in/public/v2/users/6895773',
+            headers: {
+                Authorization: "Bearer 78917e46595309accc116964733d049c33d5f78f9caf186981cd757250cf0367"
+            }
+        })
+            .then((response) => {
+                cy.log(JSON.stringify(response))
+                expect(response.status).to.equal(200)
+                expect(response.body.id).to.equal(689577)
+            })
+
     })
-    
-})
 
 })
