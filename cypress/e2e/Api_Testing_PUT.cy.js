ba24e0502f9709e39291b030ed03777e2434a3cf
Shubham Badwial <shubhambadiwal8520@gmail.com>
2024-05-09 05:58:58 +0530

    Updated the flow

--- cypress/e2e/Api_Testing_PUT.cy.js
+++ cypress/e2e/Api_Testing_PUT.cy.js
@@ -57,8 +57,7 @@ describe(' PUT Method ', () => {
                 method: 'POST',
                 url: 'https://gorest.co.in/public/v2/users',
                 headers: {
-
-                    Authorization: 'Bearer 96096fd0358d8c24b76ba3e3e415aae7102a4452fdf0ad819c070462c474b70b'
+                    Authorization: "Bearer 78917e46595309accc116964733d049c33d5f78f9caf186981cd757250cf0367"
                 },
                 body: users
             }).then((response) => {
@@ -67,8 +66,7 @@ describe(' PUT Method ', () => {
                     method: 'PUT',
                     url: 'https://gorest.co.in/public/v2/users/' + id,
                     headers: {
-
-                        Authorization: 'Bearer 96096fd0358d8c24b76ba3e3e415aae7102a4452fdf0ad819c070462c474b70b'
+                        Authorization: "Bearer 78917e46595309accc116964733d049c33d5f78f9caf186981cd757250cf0367"
                     },
                     body: updateusers
                 })
@@ -80,7 +78,7 @@ describe(' PUT Method ', () => {
                     url: 'https://gorest.co.in/public/v2/users/' + id,
                     headers: {
 
-                        Authorization: 'Bearer 96096fd0358d8c24b76ba3e3e415aae7102a4452fdf0ad819c070462c474b70b'
+                        Authorization: "Bearer 78917e46595309accc116964733d049c33d5f78f9caf186981cd757250cf0367"
                     },
                 })
                     .then((response) => {
