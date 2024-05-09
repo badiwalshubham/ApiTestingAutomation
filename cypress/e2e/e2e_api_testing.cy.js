cd398c7b12c9ef030e09f4a85b86262f8d1acc45
Shubham Badwial <shubhambadiwal8520@gmail.com>
2024-05-09 14:25:20 +0530

    E2E flow is impleted using command.js, more optimal testing flow

---
+++ cypress/e2e/e2e_api_testing.cy.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress"/>
+
+describe('API Automation ', () => {
+
+
+    it('Custom Commands', () => {
+
+
+        let payload = {
+            "name": "AB Test 01",
+            "email": "AbTest856856@dispostable.com",
+            "gender": "female",
+            "status": "active"
+        }
+
+        cy.postAPI(payload).then((response) => {
+            expect(response.status).to.be.equal(201)
+            let userId = response.body.id
+            cy.getAPI(userId).then((response) => {
+                expect(response.status).to.be.equal(200)
+            })
+            cy.putAPI(userId).then((response) => {
+                expect(response.status).to.be.equal(200)
+            })
+            cy.deleteAPI(userId).then((response) => {
+                expect(response.status).to.be.equal(204)
+            })
+
+
+
+        })
+    })
+
+
+
+
+})
+
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,12 +24,56 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-CypressTestComponentRenderer.Commands.add('getAPI', (url, headers) =>{
-   cy.request({
-    method: 'GET',
-    url: url,
-    headers: headers
-   })
+
+
+Cypress.Commands.add('getAPI', (pathParam) => {
+
+    cy.request({
+        method: 'GET',
+        url: 'https://gorest.co.in/public/v2/users/' + pathParam,
+        headers: {
+
+            Authorization: "Bearer 78917e46595309accc116964733d049c33d5f78f9caf186981cd757250cf0367"
+        }
+    })
+
+
 })
 
-Cypress.Commands.add()
+Cypress.Commands.add('postAPI', (payload) => {
+
+    cy.request({
+        method: 'POST',
+        url: 'https://gorest.co.in/public/v2/users/',
+        headers: {
+            Authorization: "Bearer 78917e46595309accc116964733d049c33d5f78f9caf186981cd757250cf0367"
+        },
+        body: payload
+
+    })
+})
+
+Cypress.Commands.add('putAPI', (pathParam) => {
+
+    cy.request({
+        method: 'PUT',
+        url: 'https://gorest.co.in/public/v2/users/' + pathParam,
+        headers: {
+
+            Authorization: "Bearer 78917e46595309accc116964733d049c33d5f78f9caf186981cd757250cf0367"
+        },
+    })
+
+})
+
+Cypress.Commands.add('deleteAPI', (pathParam) => {
+    cy.request({
+        method: 'DELETE',
+        url: 'https://gorest.co.in/public/v2/users/' + pathParam,
+        headers: {
+
+            Authorization: "Bearer 78917e46595309accc116964733d049c33d5f78f9caf186981cd757250cf0367"
+        },
+
+    })
+})
